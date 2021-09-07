0d95cea8876bcfa8ecf532d3e7bef64cc3f44625
ChrysopoeiaClasses <90039907+ChrysopoeiaClasses@users.noreply.github.com>
2021-09-07 16:40:25 +0530

    version 2.0

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 $(".admission-button").click(function() {
   alert("Welcome to the Admissions Section!")
 });
+
+const hamburger_menu = document.querySelector(".hamburger-menu")
+const container = document.querySelector(".new-container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+})
